153d67e80a14221561064cbf35d072182c70c5a3
Henri Vandersleyen <hvandersleyen@duck.com>
2022-04-01 07:47:39 -0700

    still need to figure out that hmac error

--- frontend/src/routes/api/auth/_utils.ts
+++ frontend/src/routes/api/auth/_utils.ts
@@ -1,11 +1,11 @@
-import { serialize } from "cookie";
+import { serialize } from 'cookie';
 
-import Iron from "@hapi/iron";
-import { variables } from "$lib/variables";
+import Iron from '@hapi/iron';
+import { variables } from '$lib/variables';
 
 const ENCRYPTION_SECRET = variables.encryptionSecret;
 const SESSION_LENGTH_MS = 604800000;
-export const SESSION_NAME = "session";
+export const SESSION_NAME = 'session';
 
 async function encrypt(data): Promise<string> {
 	return data && Iron.seal(data, ENCRYPTION_SECRET, Iron.defaults);
@@ -23,18 +23,23 @@ export async function createSessionCookie(data: any): Promise<string> {
 		expires: new Date(Date.now() + SESSION_LENGTH_MS),
 		httpOnly: true,
 		secure: true, //variables.currentState !== "dev"
-		path: "/",
-		sameSite: "lax"
+		path: '/',
+		sameSite: 'lax'
 	});
 }
 
 export async function getSession<T>(cookie: string): Promise<T> {
-	return await decrypt(cookie);
+	try {
+		return await decrypt(cookie);
+	} catch (e) {
+		//TODO::consider calling logout functiuon when this happen
+		console.warn(e);
+	}
 }
 
 export function removeSessionCookie(): string {
-	return serialize(SESSION_NAME, "", {
+	return serialize(SESSION_NAME, '', {
 		maxAge: -1,
-		path: "/"
+		path: '/'
 	});
 }
